0b2f296f8bd79cc6326796b180d4703acbc47933
didierrevelo <1829@holbertonschool.com>
2021-05-03 22:25:28 -0500

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Write a Python script that, using this REST API, 
-for a given employee ID, returns information about 
+"""Write a Python script that, using this REST API,
+for a given employee ID, returns information about
 his/her TODO list progress."""
-from json import loads
 from requests import get
 from sys import argv
 
