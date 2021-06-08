303c09d29efbc038db75de4018fc2aa0b1f37091
didierrevelo <1829@holbertonschool.com>
2021-06-08 17:57:30 -0500

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pythoun3
+"""[summary]
+
+    Returns:
+        [type]: [description]
+    """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/' + subreddit +'/about.json'
+    User_Agent = {'User-agent': 'rouner'}
+    subreddit_req = requests.get(url, headers=User_Agent).json().get('data')
+    if subreddit_req is None:
+        return 0
+    subreddit_req.get('subscribers')
